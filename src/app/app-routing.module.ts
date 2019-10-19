69cea5c0d35ffd06640e0264a7b93e26f4b10109
sampath <sampath.skt@gmail.com>
2019-10-19 15:56:12 +0530

    added router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CropsComponent } from './crops/crops.component'
+import { StagesComponent } from './stages/stages.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/crops', pathMatch: 'full' },
+    { path: 'crops', component: CropsComponent },
+    { path: 'stages', component: StagesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
